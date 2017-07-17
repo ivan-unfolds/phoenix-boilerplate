235172a5b4b087bb51fc51e6a6c4e5e5b9b4e450
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-07-17 11:20:18 +0100

    Add postcss plugins to brunch-config

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,7 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js'
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -20,10 +20,10 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css'
     },
     templates: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js'
     }
   },
 
@@ -37,9 +37,9 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ['static', 'css', 'js', 'vendor'],
     // Where to compile files to
-    public: "../priv/static"
+    public: '../priv/static'
   },
 
   // Configure your plugins
@@ -47,16 +47,24 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')(),
+        require('postcss-import')(),
+        require('postcss-custom-media')(),
+        require('postcss-custom-properties')()
+      ]
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["js/app"]
+      'js/app.js': ['js/app']
     }
   },
 
   npm: {
     enabled: true
   }
-};
+}
